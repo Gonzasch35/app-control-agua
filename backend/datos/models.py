961954d45b798c5ea7309bd3fd7b5d6b1e119463
Gonzasch35 <gonza.sch35@gmail.com>
2024-06-03 12:18:09 -0300

    commit

--- backend/datos/models.py
+++ backend/datos/models.py
@@ -13,24 +13,27 @@ class Medidor(models.Model):
     medidor = models.CharField(max_length=20, primary_key=True, blank=True)
     cliente = models.CharField(max_length=100, default='', blank=True)
     fecha_instalacion = models.DateField(null=True, blank=True)
-    lote = models.IntegerField(blank=True)
-    manzana = models.IntegerField(blank=True)
+    lote = models.CharField(max_length=200, blank=True)
+    manzana = models.CharField(max_length=200, blank=True)
     loteo_name = models.ForeignKey(Loteo, on_delete=models.CASCADE, related_name='medidor_loteo', default=1)
-    lectura_anterior = models.IntegerField(default=0)
-    lectura_actual = models.IntegerField(default=0)
+    lectura_anterior = models.IntegerField(null=True, blank=True)
+    lectura_actual = models.IntegerField(null=True, blank=True)
     consumo = models.IntegerField(default=0)
 
     def __str__(self):
         return f"Medidor {self.medidor} de {self.dueño}"
 
-    """ def save(self, *args, **kwargs):
-
+    def save(self, *args, **kwargs):
         # Si la instancia ya existe en la base de datos
         if self.pk is not None:
-            # Obtén la instancia actual desde la base de datos
-            existing_medidor = Medidor.objects.get(pk=self.pk)
-            # Actualiza lectura_anterior con el valor de lectura_actual de la base de datos
-            self.lectura_anterior = existing_medidor.lectura_actual
+            try:
+                # Obtén la instancia actual desde la base de datos
+                existing_medidor = Medidor.objects.get(pk=self.pk)
+                # Actualiza lectura_anterior con el valor de lectura_actual de la base de datos
+                self.lectura_anterior = existing_medidor.lectura_actual
+            except Medidor.DoesNotExist:
+                # Si no existe, inicializa lectura_anterior a 0
+                self.lectura_anterior = 0
 
-        self.consumo = self.lectura_actual - self.lectura_anterior
-        super(Medidor, self).save(*args, **kwargs) """
+        self.consumo = int(self.lectura_actual) - int(self.lectura_anterior)
+        super(Medidor, self).save(*args, **kwargs)
--- backend/load_csv.py
+++ backend/load_csv.py
@@ -13,6 +13,12 @@ def parse_date(date_str):
         return None
     return datetime.strptime(date_str, '%d/%m/%Y').date()
 
+def clean_and_convert_to_int(value):
+    try:
+        return int(value.strip())
+    except ValueError:
+        return 0  # Devuelve 0 si no se puede convertir a entero
+
 def load_csv_to_db(csv_filename):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     csv_filepath = os.path.join(base_dir, csv_filename)
@@ -21,15 +27,19 @@ def load_csv_to_db(csv_filename):
         reader = csv.DictReader(csvfile, delimiter=';')
         for row in reader:
             print(row)
+            medidor_val = row['medidor'][-4:]
+            loteo_instance = Loteo.objects.get(pk=2)  # Obtiene la instancia de Loteo, ajusta según sea necesario
+            
             medidor, created = Medidor.objects.get_or_create(
-                medidor=row['medidor'],
+                medidor=medidor_val,
                 defaults={
                     'cliente': row['cliente'],
                     'fecha_instalacion': parse_date(row['fecha_instalacion']),
                     'lote': row['lote'],
                     'manzana': row['manzana'],
-                    'lectura_anterior': row['lectura_anterior'],
-                    'lectura_actual': row['lectura_actual']
+                    'lectura_anterior': clean_and_convert_to_int(row['lectura_anterior']),
+                    'lectura_actual': clean_and_convert_to_int(row['lectura_actual']),
+                    'loteo_name': loteo_instance
                 }
             )
             if not created:
@@ -38,12 +48,28 @@ def load_csv_to_db(csv_filename):
                 medidor.fecha_instalacion = parse_date(row['fecha_instalacion'])
                 medidor.lote = row['lote']
                 medidor.manzana = row['manzana']
-                medidor.lectura_anterior = row['lectura_anterior']
-                medidor.lectura_actual = row['lectura_actual']
+                medidor.lectura_anterior = clean_and_convert_to_int(row['lectura_anterior'])
+                medidor.lectura_actual = clean_and_convert_to_int(row['lectura_actual'])
+                medidor.loteo_name = loteo_instance
                 medidor.save()
 
             print(f"Medidor {row['medidor']} guardado con éxito")
 
 if __name__ == "__main__":
-    csv_filepath = 'Libro1.csv'  # Cambia esto a la ruta de tu archivo CSV
+    csv_filepath = 'Libro2.csv'  # Cambia esto a la ruta de tu archivo CSV
     load_csv_to_db(csv_filepath)
+
+
+
+
+""" def update_consumo():
+    medidores = Medidor.objects.all()
+    for medidor in medidores:
+        # Calcula el consumo
+        medidor.consumo = medidor.lectura_actual - medidor.lectura_anterior
+        # Guarda la instancia con el consumo actualizado
+        medidor.save()
+        print(f"Medidor {medidor.medidor}: consumo actualizado a {medidor.consumo}")
+
+if __name__ == "__main__":
+    update_consumo() """
